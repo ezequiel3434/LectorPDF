7ac7a69cd574e00917f771dd40c166df60c06566
Ezequiel <ezequiel3434@gmail.com>
2020-07-04 15:36:47 -0300

    delegate y datasource de la tableview

--- LectorPDF/ViewController.swift
+++ LectorPDF/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    
+    
+    @IBOutlet var tabla: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tabla.dataSource = self
+        tabla.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = UITableViewCell()
+        celda.textLabel?.text = "Ezequiel"
+        return celda
     }
 
+    
 
 }
+
+
 
